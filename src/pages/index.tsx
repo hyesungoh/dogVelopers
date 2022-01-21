d80aa12b0f6a6aab6513ec6a7663ddd67a8a9e9f
Hyesung Oh <haesungoh414@gmail.com>
2022-01-21 19:43:40 +0900

    env: install framer-motion

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,14 @@
+import styled from '@emotion/styled';
+import { motion } from 'framer-motion';
+
 function Home() {
-  return <div>asdfasdf</div>;
+  return <Div whileHover={{ scale: 1.2 }}>asdfasdf</Div>;
 }
 
 export default Home;
+
+const Div = styled(motion.div)`
+  width: 100px;
+  height: 100px;
+  background-color: ${({ theme }) => theme.color.black};
+`;
