eae41fc4e0d67c70f0b86e579041fc19d815d698
Hyesung Oh <haesungoh414@gmail.com>
2022-01-21 18:11:03 +0900

    env: setting _document

---
+++ src/pages/_document.tsx
@@ -0,0 +1,47 @@
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
+
+class DogvelopersDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="ko">
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <meta property="og:image" content="/vercel.svg" />
+
+          <meta httpEquiv="Content-type" content="text/html; charset=utf-8" />
+          <meta property="og:title" content="동아리 개발자들" />
+          <meta property="og:description" content="동아리 개발자들" />
+
+          {/* font */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;400;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default DogvelopersDocument;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
-import type { NextPage } from 'next';
-
-const Home: NextPage = () => {
-  return <div>hello</div>;
-};
+function Home() {
+  return <div>asdfasdf</div>;
+}
 
 export default Home;
