cf91b35423f92e775c213b4845e03c61e66af490
Hyesung Oh <haesungoh414@gmail.com>
2022-02-05 21:01:02 +0900

    feat: apply hotjar

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -49,6 +49,20 @@ class DogvelopersDocument extends Document {
               gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');`,
             }}
           />
+
+          {/* for hotjar */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:${process.env.NEXT_PUBLIC_HJ_ID},hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+            }}
+          ></script>
         </Head>
         <body>
           <Main />
