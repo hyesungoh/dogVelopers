06b739943e7ccbaf5c113e1a5133e0d206869170
Hyesung Oh <haesungoh414@gmail.com>
2022-02-25 21:04:02 +0900

    [#12] apply bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
+const withPlugins = require('next-compose-plugins');
 const { withSentryConfig } = require('@sentry/nextjs');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -13,4 +17,15 @@ const sentryWebpackPluginOptions = {
   silent: true,
 };
 
-module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
+module.exports = withPlugins(
+  [
+    withSentryConfig(moduleExports, sentryWebpackPluginOptions),
+    withBundleAnalyzer({
+      compress: true,
+      webpack(config) {
+        return { ...config };
+      },
+    }),
+  ],
+  nextConfig
+);
